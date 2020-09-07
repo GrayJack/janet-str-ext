b6e02020dcb812d4bc4538959b52dda662f82afd
GrayJack <gr41.j4ck@gmail.com>
2020-09-07 03:30:11 -0300

    Comments to docs generations

--- docs.sh
+++ docs.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# build code first
 jpm build
+
+# stash changes if it exist
+git stash
+
+# site generation
 mdz
 mv site /tmp
 git checkout gh-pages
 mv /tmp/site/* .
+
+# Commit changes and go back to main branch
 git add . && git commit -m "Update the docs" && git push
 git checkout master
 rm -rf /tmp/site
