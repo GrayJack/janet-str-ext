8cfdd8c5b2f8ffed4b34d5e3968083877115f7e3
GrayJack <gr41.j4ck@gmail.com>
2021-08-31 21:27:56 -0300

    Update to janetrs 0.4.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use janetrs::{
-    janet_fn, janet_mod, jpanic, tuple, util::check_fix_arity, Janet, JanetString, JanetTuple,
-    TaggedJanet,
+    declare_janet_mod, janet_fn, jpanic, tuple, util::check_fix_arity, Janet, JanetString,
+    JanetTuple, TaggedJanet,
 };
 
-#[janet_fn]
+/// (str-ext/contains? pattern str-or-buff)
+///
+/// Returns true if and only if this `string|buffer` contains the given `pattern`. A
+/// pattern must be string|buffer as well."
+#[janet_fn(arity(fix(2)))]
 pub fn contains(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 2);
-
     match args[1].unwrap() {
         TaggedJanet::Buffer(buf) => match args[0].unwrap() {
             TaggedJanet::Buffer(pattern) => buf.contains(pattern).into(),
@@ -31,10 +33,11 @@ pub fn contains(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/ascii? str-or-buff)
+///
+/// Returns true if and only if every byte in this `string|buffer` is ASCII.
+#[janet_fn(arity(fix(1)))]
 pub fn is_ascii(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.is_ascii().into(),
         TaggedJanet::String(s) => s.is_ascii().into(),
@@ -45,10 +48,11 @@ pub fn is_ascii(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/utf8? str-or-buff)
+///
+/// Returns true if and only if the entire `string|buffer` is valid UTF-8.
+#[janet_fn(arity(fix(1)))]
 pub fn is_utf8(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.is_utf8().into(),
         TaggedJanet::String(s) => s.is_utf8().into(),
@@ -59,10 +63,12 @@ pub fn is_utf8(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/chars str-or-buff)
+///
+/// Returns a tuple with the chars of the `string|buffer`. If invalid UTF-8 is
+/// encountered, then the Unicode replacement codepoint is yielded instead.
+#[janet_fn(arity(fix(1)))]
 pub fn chars(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.chars().collect::<JanetTuple>().into(),
         TaggedJanet::String(s) => s.chars().collect::<JanetTuple>().into(),
@@ -73,10 +79,12 @@ pub fn chars(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/fields str-or-buff)
+///
+/// Returns a tuple with the fields of the `string|buffer` separated by contiguous
+/// whitespace.
+#[janet_fn(arity(fix(1)))]
 pub fn fields(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf
             .fields()
@@ -95,10 +103,12 @@ pub fn fields(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/graphemes str-or-buff)
+///
+/// Returns a tuple with the grapheme clusters of the `string|buffer`. If invalid UTF-8 is
+/// encountered, then the Unicode replacement codepoint is yielded instead.
+#[janet_fn(arity(fix(1)))]
 pub fn graphemes(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.graphemes().collect::<JanetTuple>().into(),
         TaggedJanet::String(s) => s.graphemes().collect::<JanetTuple>().into(),
@@ -109,10 +119,12 @@ pub fn graphemes(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/lines str-or-buff)
+///
+/// Returns a tuple with the lines of the `string|buffer` without the terminator. This
+/// only recognizes `\\n` and `\\r\\n` as line terminator.
+#[janet_fn(arity(fix(1)))]
 pub fn lines(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf
             .lines()
@@ -131,10 +143,12 @@ pub fn lines(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/lines-with-terminator str-or-buff)
+///
+/// Returns a tuple with the lines of the `string|buffer` with the terminators. This only
+/// recognizes `\\n` and `\\r\\n` as line terminator.
+#[janet_fn(arity(fix(1)))]
 pub fn lines_with_terminator(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf
             .lines_with_terminator()
@@ -153,10 +167,14 @@ pub fn lines_with_terminator(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/sentences str-or-buff)
+///
+/// Returns a tuple with the sentences of the `string|buffer`. Typically, a sentence will
+/// include its trailing punctuation and whitespace.
+///
+/// When invalid UTF-8 is encountered, replacement codepoints are substituted.
+#[janet_fn(arity(fix(1)))]
 pub fn sentences(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.sentences().collect::<JanetTuple>().into(),
         TaggedJanet::String(s) => s.sentences().collect::<JanetTuple>().into(),
@@ -167,10 +185,12 @@ pub fn sentences(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/words str-or-buff)
+///
+/// Returns a tuple with the words of the `string|buffer`. If invalid UTF-8 is
+/// encountered, then the Unicode replacement codepoint is yielded instead.
+#[janet_fn(arity(fix(1)))]
 pub fn words(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.words().collect::<JanetTuple>().into(),
         TaggedJanet::String(s) => s.words().collect::<JanetTuple>().into(),
@@ -181,10 +201,12 @@ pub fn words(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/words-with-breaks str-or-buff)
+///
+/// Returns a tuple with the words and breaks of the `string|buffer`. If invalid UTF-8 is
+/// encountered, then the Unicode replacement codepoint is yielded instead.
+#[janet_fn(arity(fix(1)))]
 pub fn words_with_breaks(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => buf.words_with_breaks().collect::<JanetTuple>().into(),
         TaggedJanet::String(s) => s.words_with_breaks().collect::<JanetTuple>().into(),
@@ -195,10 +217,15 @@ pub fn words_with_breaks(args: &mut [Janet]) -> Janet {
     }
 }
 
-#[janet_fn]
+/// (str-ext/utf8-chunks str-or-buff)
+///
+/// Returns a tuple with the first value being a tuple of valid UTF-8 chunks and the
+/// second value beinf a tuple of invalid UTF-8 chunks.
+///
+/// Invalid UTF-8 bytes are always 1-3 bytes, which are determined via the "substitution
+/// of maximal subparts"
+#[janet_fn(arity(fix(1)))]
 pub fn utf8_chunks(args: &mut [Janet]) -> Janet {
-    check_fix_arity(args, 1);
-
     match args[0].unwrap() {
         TaggedJanet::Buffer(buf) => {
             let (mut valid, mut invalid) = (Vec::with_capacity(10), Vec::with_capacity(10));
@@ -221,7 +248,7 @@ pub fn utf8_chunks(args: &mut [Janet]) -> Janet {
                     .collect::<JanetTuple>()
             ]
             .into()
-        }
+        },
         TaggedJanet::String(s) => {
             let (mut valid, mut invalid) = (Vec::with_capacity(10), Vec::with_capacity(10));
 
@@ -243,7 +270,7 @@ pub fn utf8_chunks(args: &mut [Janet]) -> Janet {
                     .collect::<JanetTuple>()
             ]
             .into()
-        }
+        },
         _ => jpanic!(
             "bad slot #0, expected string|buffer, got {}",
             args[0].kind()
@@ -251,17 +278,17 @@ pub fn utf8_chunks(args: &mut [Janet]) -> Janet {
     }
 }
 
-janet_mod!("str-ext";
-    {"contains?", contains, "(str-ext/contains? pattern str-or-buff)\n\nReturns true if and only if this string|buffer contains the given pattern. A pattern must be string|buffer as well."},
-    {"ascii?", is_ascii, "(str-ext/ascii? str-or-buff)\n\nReturns true if and only if every byte in this string|buffer is ASCII."},
-    {"utf8?", is_utf8, "(str-ext/utf8? str-or-buff)\n\nReturns true if and only if the entire string|buffer is valid UTF-8."},
-    {"chars", chars, "(str-ext/chars str-or-buff)\n\nReturns a tuple with the chars of the string|buffer. If invalid UTF-8 is encountered, then the Unicode replacement codepoint is yielded instead."},
-    {"fields", fields, "(str-ext/fields str-or-buff)\n\nReturns a tuple with the fields of the string|buffer separated by contiguous whitespace."},
-    {"graphemes", graphemes, "(str-ext/graphemes str-or-buff)\n\nReturns a tuple with the grapheme clusters of the string|buffer. If invalid UTF-8 is encountered, then the Unicode replacement codepoint is yielded instead."},
-    {"lines", lines, "(str-ext/lines str-or-buff)\n\nReturns a tuple with the lines of the string|buffer without the terminator. This only recognizes `\\n` and `\\r\\n` as line terminator."},
-    {"lines-with-terminator", lines_with_terminator, "(str-ext/lines-with-terminator str-or-buff)\n\nReturns a tuple with the lines of the string|buffer with the terminators. This only recognizes `\\n` and `\\r\\n` as line terminator."},
-    {"sentences", sentences, "(str-ext/sentences str-or-buff)\n\nReturns a tuple with the sentences of the string|buffer. Typically, a sentence will include its trailing punctuation and whitespace.\n\nWhen invalid UTF-8 is encountered, replacement codepoints are substituted."},
-    {"words", words, "(str-ext/words str-or-buff)\n\nReturns a tuple with the words of the string|buffer. If invalid UTF-8 is encountered, then the Unicode replacement codepoint is yielded instead."},
-    {"words-with-breaks", words_with_breaks, "(str-ext/words-with-breaks str-or-buff)\n\nReturns a tuple with the words and breaks of the string|buffer. If invalid UTF-8 is encountered, then the Unicode replacement codepoint is yielded instead."},
-    {"utf8-chunks", utf8_chunks, "(str-ext/utf8-chunks str-or-buff)\n\nReturns a tuple with the first value being a tuple of valid UTF-8 chunks and the second value beinf a tuple of invalid UTF-8 chunks.\n\nInvalid UTF-8 bytes are always 1-3 bytes, which are determined via the \"substitution of maximal subparts\""}
+declare_janet_mod!("str-ext";
+    {"contains?", contains},
+    {"ascii?", is_ascii},
+    {"utf8?", is_utf8},
+    {"chars", chars},
+    {"fields", fields},
+    {"graphemes", graphemes},
+    {"lines", lines},
+    {"lines-with-terminator", lines_with_terminator},
+    {"sentences", sentences},
+    {"words", words},
+    {"words-with-breaks", words_with_breaks},
+    {"utf8-chunks", utf8_chunks}
 );
